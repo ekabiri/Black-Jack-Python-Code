ce11d6ddfe0ec82233eeb0ed1b841b166db26477
ekabiri <eh.kabiri.ra@gmail.com>
2014-07-11 12:08:26 -0400

    Update BlackJackGame.py

--- BlackJackGame.py
+++ BlackJackGame.py
@@ -2,9 +2,9 @@
 ****************************************************************************
 Welcome to the text-based Black Jack Game:
 
-This is an OOP based BlackJack code written by ehsan Kabiri Rahani. The writer has tried to encapsulate the code to make it easier to follow
+This is an OOP based BlackJack code written by ehsan Kabiri Rahani. 
 
-1-The rules of the game follow the Casino's version in which one of the dealer's cards are 'Face Down'(Hidden) untill when the player stands
+1-BlackJack is played in different ways; The present code followa the Casino's version in which one of the dealer's cards are 'Face Down'(Hidden) untill when the player stands
 and the hidden card will be revealed when the dealer hits
 2-This version allows splitting only once, therefore the splitted hands may not be splittable again
 3-We have only hand1 when the player does not split and hand2 will come into play when splitting takes place
